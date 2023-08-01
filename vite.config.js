2c2ebbd428a37e634d13bb0a216ac4e83c8ddeb9
fathan nasrullah <fathannasrullah0@gmail.com>
2023-08-01 12:16:02 +0700

    update configuration for root path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/project',
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  const config = {
+    base: '/',
+    plugins: [react()],
+  }
+
+  if (command !== 'serve') {
+    config.base = '/project'
+  }
+
+  return config
 })
